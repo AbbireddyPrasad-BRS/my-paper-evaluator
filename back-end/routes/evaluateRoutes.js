33a5a1fe763cc899247ef8eb544084e201f73255
Sai Prasad Abbireddy <141221857+AbbireddyPrasad-BRS@users.noreply.github.com>
2025-06-25 12:04:35 +0530

    Update evaluateRoutes.js

--- back-end/routes/evaluateRoutes.js
+++ back-end/routes/evaluateRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { evaluateAnswers } = require('../controllers/evaluateController');
 
 // ✅ Define POST /api/evaluate (not /evaluate/evaluate)
-router.post('/', evaluateAnswers);
-
+// router.post('/', evaluateAnswers);
+router.post('/:rollNumber', evaluateAnswers);
 module.exports = router;
